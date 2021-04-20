edf50e50d99a3e2b39cdbd54c30675b75c311c96
pengwang7 <wangpeng19960620@163.com>
2021-04-20 20:39:21 +0800

    Add a new RpcPacket to replace rpc_msg_hdr.

--- src/rpc_msg_hdr.h
+++ src/rpc_msg_hdr.h
@@ -25,6 +25,8 @@
 #ifndef __MESSAGE_HEADER_H__
 #define __MESSAGE_HEADER_H__
 
+#include <endian.h>
+
 namespace drpc {
 
 enum MessageContentType {
@@ -33,7 +35,103 @@ enum MessageContentType {
     MSG_CONTENT_TYPE_PROTOBUF = 1,
 };
 
+enum RpcPayloadType {
+    PAYLOAD_TYPE_JSON = 0,
+
+    PAYLOAD_TYPE_PROTOBUF = 1,
+};
+
 #pragma pack(1)
+
+class RpcPacket {
+public:
+    struct Header {
+#if (__BYTE_ORDER == __LITTLE_ENDIAN)
+        uint8_t padding:1;
+        uint8_t extension:3;
+        uint8_t version:4;
+        uint8_t payload:7;
+        uint8_t marker:1;
+#elif (__BYTE_ORDER == __BIG_ENDIAN)
+        uint8_t version:4;
+        uint8_t padding:1;
+        uint8_t extension:3;
+        uint8_t marker:1;
+        uint8_t payload:7;
+#endif
+        uint32_t length;
+    };
+
+    struct Body {
+        std::string content;
+    };
+
+public:
+    static RpcPacket* CreateRpcPacket(Buffer& buffer, ByteBufferReader* io_reader) {
+        Header header;
+        memset(&header, 0, sizeof(header));
+
+        if (!io_reader->ReadBytes(reinterpret_cast<char*>(&header), sizeof(header))) {
+            DWARNING("The buffer unread bytes < sizeof(header).");
+            return nullptr;
+        }
+
+        uint32_t body_length = static_cast<uint32_t>(ntohl(header.length));
+
+        //DDEBUG("payload_type: {}, body_length: {}.", header.payload, body_length);
+
+        Body body;
+
+        if (buffer.UnreadByteSize() < body_length + sizeof(header)) {
+            DWARNING("Data is not complete.");
+            return nullptr;
+        }
+
+        io_reader->Consume(sizeof(header));
+
+        if (!io_reader->ReadString(&body.content, body_length)) {
+            DERROR("Read data from buffer failed.");
+            return nullptr;
+        }
+
+        io_reader->Consume(body_length);
+
+        RpcPacket* packet = new RpcPacket(header, body);
+
+        return packet;
+    }
+
+    RpcPacket(Header header, Body body) : header_(header), body_(body) {
+        DTRACE("Construct RpcPacket.");
+    }
+
+    ~RpcPacket() {
+        DTRACE("Destructor RpcPacket.");
+    }
+
+public:
+    uint8_t GetVersion() {
+        return header_.version;
+    }
+
+    uint8_t GetPayloadType() {
+        return header_.payload;
+    }
+
+    uint32_t GetBodyLength() {
+        return static_cast<uint32_t>(ntohl(header_.length));
+    }
+
+    std::string GetBody() {
+        return body_.content;
+    }
+
+private:
+    Header header_;
+
+    Body body_;
+};
+
 struct rpc_msg_hdr {
     uint8_t version:2;
 
